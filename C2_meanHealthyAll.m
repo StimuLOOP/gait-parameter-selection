d358e01e42fd10a5742d8bccddfee18d30868c83
MathildeISIR <mathilde.lestoille@gmail.com>
2022-07-20 12:32:34 +0200

    add plot to visualize the metrics

--- C2_meanHealthyAll.m
+++ C2_meanHealthyAll.m
@@ -1,6 +1,8 @@
 %--------------------------------
 % Compute mean for all healthy participants (take individual step-by-step values from all
 % healthy participants and compute stat from them)
+% Possibility to draw some barplots to visualize the metrics
+% For discrete parameters
 % Mathilde 18.07.2022
 % ------------------------------
 
@@ -15,6 +17,9 @@ speeds = {'0.11'; '0.19';'0.28';'0.36';'0.42';'0.53';'0.61';'0.69';'0.78';'0.86'
 fileMatlab = 'D:\StimuLOOP\DataGait\NM_Reference\statHealthy.mat';
 load(fileMatlab);
 
+saveFile = 0;
+plotFigures =1;
+varToPlot = 'RStanceTime';
 
 for speedN = 1:size(speeds,1)
     %% Gather all healthy values in one matrix for the different parameters
@@ -27,11 +32,39 @@ for speedN = 1:size(speeds,1)
     end
 
     %% Compute mean, std and coeff of variation
-    meanAllHealthy = structfun(@mean,healthy,'UniformOutput',false);
+    meanAllHealthy{speedN,1} = structfun(@mean,healthy,'UniformOutput',false);
     stdAllHealthy{speedN,1} = structfun(@(x)(std(x,0,1)),healthy,'UniformOutput',false);
     varAllHealthy{speedN,1} = struct(fields{:});
-    for i = 1: size(fields,1)
+    for k = 1: size(fields,1)
         varAllHealthy{speedN,1}.(fields{k}) = stdAllHealthy{speedN,1}.(fields{k})./meanAllHealthy{speedN,1}.(fields{k});
     end
 
+    %% Bar plots
+    if plotFigures
+        names = {'Max';'Min';'ROM';'SwMax';'SwMin';'SwROM';'StMax';'StMin';'StROM'};
+        
+        a = [varToPlot,'Max'];
+        try  meanAllHealthy{speedN}.(a); % see if it is a joint param (max,min,rom + sw and stance) or a spatiotemporal one (only one value)
+            figure(speedN); hold on;
+            for i = 1:9
+                a = [varToPlot,names{i}];
+                subplot(3,3,i); 
+                barweb(meanAllHealthy{speedN}.(a)', stdAllHealthy{speedN}.(a)');
+                axis([0.5 1.5 -inf inf]);
+                title(a);
+                xticklabels('');
+            end
+        catch
+            figure(1); hold on;
+            subplot(2,7,speedN);
+            barweb(meanAllHealthy{speedN}.(varToPlot)', stdAllHealthy{speedN}.(varToPlot)');
+            axis([0.5 1.5 -inf inf]);
+                title(a);
+                xticklabels('');
+        end
+    end
+end
+
+if saveFile
+    save(fileMatlab,'meanAllHealthy','stdAllHealthy','varAllHealthy','-append');
 end
