dbea751ac95bd6c4e72accf7477da4db4db8772c
Lestoille  Mathilde <mlestoille@ethz.ch>
2022-07-26 15:40:41 +0200

    add possibility to run the zscore selection with healthy participants data

--- D1_zscore.m
+++ D1_zscore.m
@@ -9,13 +9,20 @@ clear all
 close all
 clc
 
-% Define parameters/ file characteristics for patients
-day = {'190913','','190924','191003','191004','191004','191022','191023'};
-speeds = {'FST0.15' 'PGV0.1' ''; '' '' '';'FST0.7' 'PGV0.4' 'SLW0.4'; 'FST0.15' 'PGV0.05' '';'FST0.75' 'PGV0.55' 'SLW0.3'; 'PGV0.25' 'PGV0.5' 'PGV0.8'; 'FST0.35' 'PGV0.20' 'SLW0.07crop'; 'FST0.3' 'PGV0.17' 'SLW0.08'};
-velP = [0.15 0.1 0; 0 0 0; 0.7 0.4 0.4; 0.15 0.05 0; 0.75 0.55 0.3; 0.25 0.5 0.8; 0.35 0.20 0.07; 0.3 0.17 0.08];
-assist ={'_NH_NS' '_NRH_NS1' '';'' '' ''; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'};
-suffixe = '_MoCgapfilled';
+type = 'patient';
 
+if strcmp(type,'patient')
+    % Define parameters/ file characteristics for patients
+    day = {'190913','','190924','191003','191004','191004','191022','191023'};
+    speeds = {'FST0.15' 'PGV0.1' ''; '' '' '';'FST0.7' 'PGV0.4' 'SLW0.4'; 'FST0.15' 'PGV0.05' '';'FST0.75' 'PGV0.55' 'SLW0.3'; 'PGV0.25' 'PGV0.5' 'PGV0.8'; 'FST0.35' 'PGV0.20' 'SLW0.07crop'; 'FST0.3' 'PGV0.17' 'SLW0.08'};
+    velP = [0.15 0.1 0; 0 0 0; 0.7 0.4 0.4; 0.15 0.05 0; 0.75 0.55 0.3; 0.25 0.5 0.8; 0.35 0.20 0.07; 0.3 0.17 0.08];
+    assist ={'_NH_NS' '_NRH_NS1' '';'' '' ''; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'; '_BH_NS' '_BH_NS' '_BH_NS'};
+    suffixe = '_MoCgapfilled';
+
+elseif strcmp(type,'heathy')
+    day = {'200113';'200302';'200306';'200306';'200306';'200306';'200617'};
+    speeds = {'0.11'; '0.19';'0.28';'0.36';'0.42';'0.53';'0.61';'0.69';'0.78';'0.86';'0.94';'1.03';'1.11'};
+end
 velH = [0.11 0.19 0.28 0.36 0.42 0.53 0.61 0.69 0.78 0.86 0.94 1.03 1.11]; % velocities healthy participants
 
 subject = 5;
@@ -24,16 +31,23 @@ subject = 5;
 % Mean healthy
 fileHealthy = 'D:\StimuLOOP\DataGait\NM_Reference\statHealthy.mat';
 load(fileHealthy);
-% Patient
-if subject == 4
-    subjectN = [day{subject},'_S0',num2str(subject),'_T2'];
-else
-    subjectN = [day{subject},'_S0',num2str(subject)];
-end
-folder = ['D:\StimuLOOP\DataGait\NM_GaitSegmentation\',subjectN,'\04_Visual3D\'];
-filePatient = [folder,'MatlabData\',subjectN,'_parameters'];
-load(filePatient);
 
+if strcmp(type,'patient')
+    % Patient
+    if subject == 4
+        subjectN = [day{subject},'_S0',num2str(subject),'_T2'];
+    else
+        subjectN = [day{subject},'_S0',num2str(subject)];
+    end
+    folder = ['D:\StimuLOOP\DataGait\NM_GaitSegmentation\',subjectN,'\04_Visual3D\'];
+    filePatient = [folder,'MatlabData\',subjectN,'_parameters'];
+    load(filePatient);
+
+elseif strcmp(type,'healthy')
+    folder = ['D:\StimuLOOP\DataGait\NM_Reference\',subjectN,'\04_Visual3D\'];
+    fileHealthy = [folder,'MatlabData',day{subject},'_',subjectN,'_parameters'];
+    load(fileHealthy);
+end
 dimName = {' /X','Y','Z'};
 
 for speedN = 1:size(speeds,2)
@@ -41,14 +55,18 @@ for speedN = 1:size(speeds,2)
         % do nothing
     else
         % find the corresponding velocity of healthy participants (closest one)
-        [minDistance,indOfMin] = min(abs(velP(subject,speedN)-velH));
+        if strcmp(type,'patient')
+            [minDistance,indOfMin] = min(abs(velP(subject,speedN)-velH));
+        elseif strcmp(type,'healthy')
+            indOfMin = speedN;
+        end
         % compute z-score for all metrics
         fields = fieldnames(param{speedN});
         for k = 1:size(fields,1)
             s = size(meanSubject{speedN}.(fields{k}),2);
             zsc{speedN}(k,1:s) = (meanSubject{speedN}.(fields{k})-meanHealthy{indOfMin}.(fields{k}))./stdHealthy{indOfMin}.(fields{k});
             if s<3
-            zsc{speedN}(k,s+1:3) = NaN; % put nan when there is only one dimension
+                zsc{speedN}(k,s+1:3) = NaN; % put nan when there is only one dimension
             end
         end
         % Find 4 max values
